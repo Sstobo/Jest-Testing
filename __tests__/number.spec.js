58b7290cd88e7ef24856bdcdb6f8bdcfaeb7708e
Sstobo <seanstobo@gmail.com>
2018-02-20 16:08:03 -0800

    Numbers.js letter counter initial iteration

--- __tests__/number.spec.js
+++ __tests__/number.spec.js
@@ -3,8 +3,8 @@ const number = require('./../lib/number.js');
 describe('Number array', () => {
   describe('When result is an array', () => {
     test('number', () => {
-      const result = number('wow');
-      expect(result).toEqual(['w', 'o', 'w']);
+      const result = number('wowwww');
+      expect(result).toEqual({ w: 5, o: 1 });
     });
   });
 });
